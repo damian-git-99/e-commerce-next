11fd24b778cacf619b7d6cfa65aa2f50c6fc2717
damian <damiangalvan66@gmail.com>
2024-01-03 12:25:42 -0600

    feat: add more pages and components

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,18 +1,31 @@
+import { ProductGrid, Title } from '@/components'
+import { Category } from '@/interfaces'
+import { initialData } from '@/seed/seed'
 import { notFound } from 'next/navigation'
 
 interface Props {
   params: {
-    id: string
+    id: Category
   }
 }
 
+const products = initialData.products
+
 export default function CategoryPage({ params }: Props) {
   const { id } = params
-  // todo: check if category is valid
-  if (id === 'kids') notFound()
+  const filteredProductsByCategory = products.filter((p) => p.gender === id)
+  const labels: Record<Category, string> = {
+    men: 'Men',
+    women: 'Women',
+    kid: 'Kids',
+    unisex: 'All'
+  }
+  if (filteredProductsByCategory.length === 0) notFound()
+
   return (
-    <div>
-      <h1>Category Page</h1>
-    </div>
+    <>
+      <Title title={`${labels[id]}`} subtitle="All Products" />
+      <ProductGrid products={filteredProductsByCategory} />
+    </>
   )
 }
--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,4 +1,4 @@
-import { TopMenu } from '@/components'
+import { Footer, TopMenu } from '@/components'
 import { Sidebar } from '@/components/ui/sidebar/Sidebar'
 
 export default function ShopLayout({
@@ -11,6 +11,7 @@ export default function ShopLayout({
       <TopMenu />
       <Sidebar />
       <div className="px-0 sm:px-10">{children}</div>
+      <Footer />
     </main>
   )
 }
