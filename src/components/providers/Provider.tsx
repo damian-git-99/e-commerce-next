f57e8dbbf59ffcbddc165060534c0cff69a28a9c
damian <damiangalvan66@gmail.com>
2024-01-19 10:55:24 -0600

    feat: add paypal

--- src/components/providers/Provider.tsx
+++ src/components/providers/Provider.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import { SessionProvider } from 'next-auth/react'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 interface Props {
   children: React.ReactNode
 }
 
 export const Provider = ({ children }: Props) => {
-  return <SessionProvider>{children}</SessionProvider>
+  const clientId = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID ?? ''
+  return (
+    <SessionProvider>
+      <PayPalScriptProvider
+        options={{ clientId, intent: 'capture', currency: 'USD' }}
+      >
+        {children}
+      </PayPalScriptProvider>
+    </SessionProvider>
+  )
 }
