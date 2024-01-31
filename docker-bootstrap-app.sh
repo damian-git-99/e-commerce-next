d7e696faa477f96442e2ed31d76d0451154189b9
damian <damiangalvan66@gmail.com>
2024-01-31 11:28:48 -0600

    chore(docker-bootstrap-app.sh): remove unnecessary comment about environment variables not passing through to subprocesses chore(docker-bootstrap-app.sh): remove explicit passing of DATABASE_URL to prisma migrate command as it is not needed

--- docker-bootstrap-app.sh
+++ docker-bootstrap-app.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# ENVIRONEMTN from docker-compose.yaml doesn't get through to subprocesses
-# Need to explicit pass DATABASE_URL here, otherwise migration doesn't work
 # Run migrations
 DATABASE_URL="$DATABASE_URL" npx prisma migrate deploy
 # start app
